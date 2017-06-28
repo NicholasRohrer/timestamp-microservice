be5c12167286fa57f0123117d20e0c9619f366e1
Nicholas Rohrer <nrohrer2@gmail.com>
2017-06-27 22:03:42 -0500

    changed port for Heroku deployment

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var app = module.exports = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+app.set('port', (process.env.PORT || 5000));
+
 // GET call to return JSON that formats natural and unix dates
 app.get('/dateValues/:dateVal', function(req,res,next){
     // gets the request data for date
@@ -37,6 +39,6 @@ app.get('/dateValues/:dateVal', function(req,res,next){
 });
 
 // start server
-app.listen(3000, function(){
-    console.log('Server running on port 3000');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
